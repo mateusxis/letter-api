9565470bdc4c5498dbf6ebfdc06b1b34300689f8
mateusxis <mateusx@poli.ufrj.br>
2022-09-23 15:52:30 -0300

    feat(rootRouter): create routes

---
+++ src/application/routes/root.js
@@ -0,0 +1,11 @@
+const Router = require('koa-router');
+
+module.exports = () => {
+  const router = new Router();
+
+  router.get('/liveness', async (ctx) => {
+    ctx.body = 'OK';
+  });
+
+  return router;
+};
