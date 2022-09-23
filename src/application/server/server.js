58f79800d1e30dd75474feac884a728e68ab3efd
mateusxis <mateusx@poli.ufrj.br>
2022-09-23 16:30:59 -0300

    feat(server): add config

--- src/application/server/server.js
+++ src/application/server/server.js
@@ -5,11 +5,7 @@ const cors = require('@koa/cors');
 const helmet = require('koa-helmet');
 const KoaLogger = require('koa-logger');
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
-module.exports = ({ router }) => {
+module.exports = ({ config, router }) => {
   const app = new Koa();
 
   app
@@ -22,8 +18,8 @@ module.exports = ({ router }) => {
 
   const start = () => {
     try {
-      app.listen(process.env.PORT, () => {
-        console.log(`Server listening on ${process.env.PORT}`);
+      app.listen(config.portServer, () => {
+        console.log(`Server listening on ${config.portServer}`);
       });
     } catch (err) {
       console.log('Problem initializing application dependencies');
