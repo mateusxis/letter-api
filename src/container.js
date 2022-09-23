bb5ad2cdc91b9168950715efcdcee3b0441e5690
mateusxis <mateusx@poli.ufrj.br>
2022-09-23 17:44:32 -0300

    feat(container): add requestFactory

--- src/container.js
+++ src/container.js
@@ -2,12 +2,14 @@ const { createContainer, asFunction } = require('awilix');
 
 const router = require('./application/routes');
 const server = require('./application/server');
+const requestFactory = require('./infrastructure/request');
 const configEnvs = require('./config');
 
 const container = createContainer();
 
 container.register({
   config: asFunction(configEnvs).singleton(),
+  request: asFunction(requestFactory).singleton(),
   router: asFunction(router).singleton(),
   server: asFunction(server).singleton()
 });
