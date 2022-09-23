423e3ee45897ace1f668cbc8ff35a31e1d02c0a9
mateusxis <mateusx@poli.ufrj.br>
2022-09-23 16:27:08 -0300

    feat(container): add config

--- src/container.js
+++ src/container.js
@@ -2,10 +2,12 @@ const { createContainer, asFunction } = require('awilix');
 
 const router = require('./application/routes');
 const server = require('./application/server');
+const configEnvs = require('./config');
 
 const container = createContainer();
 
 container.register({
+  config: asFunction(configEnvs).singleton(),
   router: asFunction(router).singleton(),
   server: asFunction(server).singleton()
 });
