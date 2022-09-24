66bac30402ebc159f6d5d6f701bd0ec61da04c80
mateusxis <mateusx@poli.ufrj.br>
2022-09-24 20:23:44 -0300

    feat(config): add new variables

--- src/config/config.js
+++ src/config/config.js
@@ -17,7 +17,34 @@ module.exports = () => {
     return process.env.PORT_SERVER;
   };
 
+  const getApiBaseUrl = () => {
+    const url = process.env.JSON_SERVER_API_BASE_URL;
+
+    if (url) return url;
+
+    throw new Error('"JSON_SERVER_API_BASE_URL" must be defined.');
+  };
+
+  const getApiDataRefreshInterval = () => {
+    const interval = Number(process.env.SERVICE_LETTER_DATA_REFRESH_INTERVAL_MS);
+
+    if (interval) return interval;
+
+    throw new Error('"SERVICE_LETTER_DATA_REFRESH_INTERVAL_MS" must be defined.');
+  };
+
+  const getApiRequestTimeout = () => {
+    const timeout = Number(process.env.SERVICE_LETTER_REQUEST_TIMEOUT_MS);
+
+    if (timeout) return timeout;
+
+    throw new Error('"SERVICE_LETTER_REQUEST_TIMEOUT_MS" must be defined.');
+  };
+
   return {
+    apiBaseUrl: getApiBaseUrl(),
+    apiDataRefreshInterval: getApiDataRefreshInterval(),
+    apiRequestTimeout: getApiRequestTimeout(),
     portServer: getPortServer(),
     nodeEnv: getNodeEnv()
   };
--- src/config/config.test.js
+++ src/config/config.test.js
@@ -7,19 +7,46 @@ jest.mock('dotenv', () => ({
 beforeEach(() => {
   process.env.PORT_SERVER = 3000;
   process.env.NODE_ENV = 'development';
+  process.env.JSON_SERVER_API_BASE_URL = 'https://json-server.com';
+  process.env.SERVICE_LETTER_REQUEST_TIMEOUT_MS = 5000;
+  process.env.SERVICE_LETTER_DATA_REFRESH_INTERVAL_MS = 60000;
 
   jest.resetAllMocks();
 });
 
 describe('config()', () => {
   it('should call config return variables', () => {
-    expect(config()).toEqual({ portServer: '3000', nodeEnv: 'development' });
+    expect(config()).toEqual({
+      apiBaseUrl: 'https://json-server.com',
+      apiDataRefreshInterval: 60000,
+      apiRequestTimeout: 5000,
+      portServer: '3000',
+      nodeEnv: 'development'
+    });
   });
 
   it('should set node env like "production"', () => {
     process.env.NODE_ENV = 'production';
 
     expect(config().nodeEnv).toEqual('production');
+  });
+
+  it('should throw error missing service letter data refresh interval ms variable', () => {
+    delete process.env.SERVICE_LETTER_DATA_REFRESH_INTERVAL_MS;
+
+    expect(() => config()).toThrowError('"SERVICE_LETTER_DATA_REFRESH_INTERVAL_MS" must be defined.');
+  });
+
+  it('should throw error missing service letter data request timeout ms variable', () => {
+    delete process.env.SERVICE_LETTER_REQUEST_TIMEOUT_MS;
+
+    expect(() => config()).toThrowError('"SERVICE_LETTER_REQUEST_TIMEOUT_MS" must be defined.');
+  });
+
+  it('should throw error missing json server api base url variable', () => {
+    delete process.env.JSON_SERVER_API_BASE_URL;
+
+    expect(() => config()).toThrowError('"JSON_SERVER_API_BASE_URL" must be defined.');
   });
 
   it('should throw error missing port server variable', () => {
