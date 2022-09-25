40b72cad6c5c2527a3aa9dd81973e9d29e1fcd45
mateusxis <mateusx@poli.ufrj.br>
2022-09-25 11:49:14 -0300

    feat(container): add news dependencies
    letterDomain, loggerFactory, letterService

--- src/container.js
+++ src/container.js
@@ -2,13 +2,19 @@ const { createContainer, asFunction } = require('awilix');
 
 const router = require('./application/routes');
 const server = require('./application/server');
+const letterDomain = require('./domain/letter');
 const requestFactory = require('./infrastructure/request');
+const loggerFactory = require('./infrastructure/logger');
+const letterService = require('./integrations/letterService');
 const configEnvs = require('./config');
 
 const container = createContainer();
 
 container.register({
   config: asFunction(configEnvs).singleton(),
+  letterDomain: asFunction(letterDomain).singleton(),
+  letterService: asFunction(letterService).singleton(),
+  logger: asFunction(loggerFactory).singleton(),
   request: asFunction(requestFactory).singleton(),
   router: asFunction(router).singleton(),
   server: asFunction(server).singleton()
