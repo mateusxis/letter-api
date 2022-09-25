33f3842b9276839cb90e37e16efc1808cb45759f
mateusxis <mateusx@poli.ufrj.br>
2022-09-25 11:50:38 -0300

    feat(routes): create routes
    readiness and letters

--- src/application/routes.js
+++ src/application/routes.js
@@ -1,10 +1,22 @@
 const Router = require('koa-router');
 
-module.exports = () => {
+module.exports = ({ letterDomain, letterService }) => {
   const router = new Router();
 
   router.get('/liveness', async (ctx) => {
     ctx.body = 'OK';
+  });
+
+  router.get('/readiness', async (ctx) => {
+    const isFulfilled = await letterService.isFulfilled();
+
+    ctx.body = isFulfilled ? 'OK' : 'NOK';
+  });
+
+  router.get('/letters', async (ctx) => {
+    const letters = await letterDomain.getLetters();
+
+    ctx.body = letters;
   });
 
   return router;
